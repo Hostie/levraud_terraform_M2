9ed46277b5bb426d19595c0d5a8af748574ea6de
Hostie <45092612+Hostie@users.noreply.github.com>
2023-07-04 16:32:16 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,11 @@ resource "aws_ssm_parameter" "dylan" { # changer bobby
   type  = "String"
   value = "dylan" # changer ça
 }
+
+resource "aws_instance" "web-dydy" {
+  ami           = "ami-0ebc281c20e89ba4b"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "HelloWorld-Dydy"
+  }
+}
